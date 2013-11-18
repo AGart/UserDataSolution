ba1c2962162d8e31400e2089c39bc844c9513cd1
Albee <A_Gartenberg@yahoo.com>
2013-11-18 15:11:30 -0500

    second commit

--- User Data Challenge Solution/ABGUserData.h
+++ User Data Challenge Solution/ABGUserData.h
@@ -8,13 +8,13 @@
 
 #import <Foundation/Foundation.h>
 
-@interface ABGUserData : NSObject
-
 #define USER_NAME @"username";
 #define USER_EMAIL @"email";
 #define USER_PASSWORD @"password";
 #define USER_AGE @"age";
-#define USER_PROFILE_PICTURE @"profile picture";
+#define USER_PROFILE_PICTURE @"profilePicture";
+
+@interface ABGUserData : NSObject
 
 + (NSArray *)users;
 //Returns array of NSDictionary of Users
--- User Data Challenge Solution/ABGViewController.m
+++ User Data Challenge Solution/ABGViewController.m
@@ -19,10 +19,9 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    
     self.users = [ABGUserData users];
-    NSLog(@"%@", self.users);
-    
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning
@@ -30,10 +29,23 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-/*
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.users count];
+}
+
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-
+    static NSString *cellIdentifier = @"userCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: cellIdentifier forIndexPath:indexPath];
+    
+    NSDictionary *user = self.users[indexPath.row];
+    cell.textLabel.text = [user[USER_NAME]];
+    cell.detailTextLabel.text = [user[USER_EMAIL]];
+    cell.imageView.image = [user[USER_PROFILE_PICTURE]];
+    
+    return cell;
 }
-*/
+
 @end
